fb2e73529008062ce5e3e097273bb9f403086acc
phupha <y.phupha@gmail.com>
2026-08-28 16:52:03 +0700

    ggez

--- src/Pages/Owner.jsx
+++ src/Pages/Owner.jsx
@@ -2,8 +2,13 @@ import CardRuefulEventideArt from "../assets/CardRuefulEventideArt.png";
 
 export default function Owner (){
     return(
-        <div className="flex flex-col bg-gray-300 flex-1 justify-center items-center">
-            <h1>a</h1><img src= {CardRuefulEventideArt} className="h-64 w-64"></img>
+        <div className="flex flex-col bg-gray-300 flex-1  items-center">
+            <></>
+            <h1 className="m-16 text-3xl font-bold">28 phupha (ภูผา)</h1>
+            <img src= {CardRuefulEventideArt} className="h-64 w-64"></img>
+            <h3 className="mt-12 text-2xl font-bold">short biography</h3>
+            <p className="m-12">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos magni aut quibusdam fugit eaque ut nobis soluta quae laboriosam, quis pariatur sit quo eum veniam distinctio fuga natus quos incidunt asperiores et ab sed. Illo optio cum doloremque recusandae architecto!
+            </p>
         </div>
     )
 }
